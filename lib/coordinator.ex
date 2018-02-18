f791f33f0fd6bec52acb991462baf76a7dec509b
Michael Quoma <mquoma@yahoo.com>
2018-02-18 17:33:36 -0500

    add coordinator

--- lib/coordinator.ex
+++ lib/coordinator.ex
@@ -1,2 +1,21 @@
 defmodule Metex.Coordinator do
+  def loop(results \\ [], total_count) do
+    receive do
+      {:ok, data} ->
+        new_results = [data | results]
+
+        if total_count == Enum.count(new_results) do
+          send(self, :exit)
+        end
+
+        loop(new_results, total_count)
+
+      :exit ->
+        results |> Enum.sort() |> Enum.join(", ") |> IO.inspect()
+
+      _ ->
+        IO.puts("unexpected msg")
+        loop(results, total_count)
+    end
+  end
 end
--- lib/metex.ex
+++ lib/metex.ex
@@ -3,6 +3,11 @@ defmodule Metex do
   Documentation for Metex.
   """
 
+  import Metex.Coordinator
+  import Metex.Worker
+  alias Metex.Coordinator
+  alias Metex.Worker
+
   @doc """
   Hello world.
 
@@ -14,5 +19,16 @@ defmodule Metex do
   """
   def hello do
     :world
+  end
+
+  def temperature_of(cities) do
+    total = Enum.count(cities)
+    coordinator_pid = spawn(Coordinator, :loop, [[], total])
+
+    cities
+    |> Enum.map(fn c ->
+      worker_pid = spawn(Worker, :loop_temperature, [])
+      send(worker_pid, {coordinator_pid, c})
+    end)
   end
 end
